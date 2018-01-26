9261a7ea1c92bdb90ce45943999acb4a75e7fe5f
pedro <cruzrodriguezpedroj@gmail.com>
2018-01-26 13:44:56 -0600

    seccion tres y cuatro index

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ include('recursos/head.php');
   </a>
 </div>
 <div class="container-fluid">
-  <div class="row seccion2-index">
+  <div class="row margenes-secciones">
     <div class="col-md-4">
       <div class="row">
         <div class="col-md-12 text-center">
@@ -94,6 +94,56 @@ include('recursos/head.php');
       </div>
     </div>
 
+  </div>
+</div>
+<div class="container">
+  <div class="row margenes-secciones">
+    <div class="col-md-2">
+
+    </div>
+    <div class="col-md-8">
+      <img src="img/video.jpg" alt="">
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row margenes-secciones">
+    <div class="col-md-6">
+      <div class="card" style="width: 27rem;">
+        <img class="card-img-top" src="img/pagos.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">PLAN MENSUAL</h5>
+          <p class="card-text">este plan contempla el uso de la plataforma por un perioro de un mes y con acceso total al contenido multimedia</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">renovacion cada mes</li>
+          <li class="list-group-item">10% menos </li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+        <div class="card-body">
+          <a href="#" class="card-link">Registrarse</a>
+          <a href="#" class="card-link">ver mas</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="card" style="width: 27rem;">
+        <img class="card-img-top" src="img/pagos.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">PLAN MENSUAL</h5>
+          <p class="card-text">este plan contempla el uso de la plataforma por un perioro de un mes y con acceso total al contenido multimedia</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">renovacion cada mes</li>
+          <li class="list-group-item">10% menos </li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+        <div class="card-body">
+          <a href="#" class="card-link">Registrarse</a>
+          <a href="#" class="card-link">ver mas</a>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
