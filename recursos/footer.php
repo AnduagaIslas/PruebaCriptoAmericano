b03624d790d5b457f0b4d639b3b0dc63178499c7
pedro <cruzrodriguezpedroj@gmail.com>
2018-01-26 11:40:49 -0600

    footer

--- recursos/footer.php
+++ recursos/footer.php
@@ -1,9 +1,84 @@
 
-  <div class="row">
-    <div class="col-md-12 ">
-        <h6 class="text-center">Desing by</h6>
-    </div>
+<footer>
+  <div class="footer" id="footer">
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                  <h3> Lorem Ipsum </h3>
+                  <ul>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                  </ul>
+              </div>
+              <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                  <h3> Lorem Ipsum </h3>
+                  <ul>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                  </ul>
+              </div>
+              <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                  <h3> Lorem Ipsum </h3>
+                  <ul>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                  </ul>
+              </div>
+              <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                  <h3> Lorem Ipsum </h3>
+                  <ul>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                      <li> <a href="#"> Lorem Ipsum </a> </li>
+                  </ul>
+              </div>
+              <div class="col-lg-3  col-md-3 col-sm-6 col-xs-12 ">
+                  <h3> Lorem Ipsum </h3>
+                  <ul>
+                      <li>
+                          <div class="input-append newsletter-box text-center">
+                              <input type="text" class="full text-center" placeholder="Email ">
+                              <button class="btn  bg-gray" type="button"> Lorem ipsum <i class="fas fa-arrow-right"></i> </button>
+                          </div>
+                      </li>
+                  </ul>
+                  <ul class="social">
+                      <li> <a href="#"> <i class="fab fa-facebook-f"></i> </a> </li>
+                      <li> <a href="#"> <i class="fab fa-twitter"></i> </a> </li>
+                      <li> <a href="#"> <i class="fab fa-google-plus-g"></i> </a> </li>
+                      <li> <a href="#"><i class="fab fa-youtube"></i> </a> </li>
+                  </ul>
+              </div>
+          </div>
+          <!--/.row-->
+      </div>
+      <!--/.container-->
   </div>
+  <!--/.footer-->
+
+  <div class="footer-bottom">
+      <div class="container">
+          <p class="pull-left"> Copyright © 2018. All right reserved CRIPTOAMERICANO. </p>
+          <div class="pull-right">
+              <ul class="nav nav-pills payments">
+                <li><i class="fab fa-cc-visa tarjetas-pago"></i></li>
+
+                <li><i class="fab fa-cc-mastercard tarjetas-pago"></i></li>
+
+                <li><i class="fab fa-cc-paypal tarjetas-pago"></i></li>
+              </ul>
+          </div>
+      </div>
+  </div>
+  <!--/.footer-bottom-->
+</footer>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
